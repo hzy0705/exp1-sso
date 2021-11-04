cae05545ae637e6813ad0e98a116d16903dffc36
hzy0705 <675849402@qq.com>
2021-11-04 13:41:55 +0800

    exp1-sso

---
+++ sso-login/src/main/java/com/sso/login/LoginApp.java
@@ -0,0 +1,15 @@
+package com.sso.login;
+/**
+ 登录
+ **/
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginApp.class,args);
+    }
+}
